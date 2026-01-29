1e6f8bf5735d5d568f94c5d912a6bf6ca14cc476
Cassio Ribeiro de Freitas <geo.cassio.ufu@gmail.com>
2026-01-29 17:34:40 -0300

    Fix: removido importmap e estabilizado build

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    // Mapeamos apenas a chave necessária. 
-    // Isso evita expor variáveis sensíveis do sistema (como PATH) no navegador.
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+    // Garante que a variável seja uma string mesmo se estiver vazia
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || "")
   },
   build: {
-    // Aumenta o limite de aviso para 2000kb para evitar alertas com bibliotecas grandes como Recharts e Gemini
     chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
-        // Divide as bibliotecas em pedaços menores para melhor cache e performance
         manualChunks(id) {
           if (id.includes('node_modules')) {
             if (id.includes('recharts')) {
